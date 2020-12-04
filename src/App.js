c777eedef7696da3db5ad2fc5fa112902c3f315b
Emiel de Jong <emieldejong@xs4all.nl>
2020-12-04 10:14:20 +0100

    .env.dist

--- src/App.js
+++ src/App.js
@@ -1,39 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SearchBar from './components/searchBar/SearchBar';
 import TabBarMenu from './components/tabBarMenu/TabBarMenu';
 import MetricSlider from './components/metricSlider/MetricSlider';
+import ForecastTab from './pages/forecastTab/ForecastTab';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [weatherData, setWeatherData] = useState(null);
+  const [location, setLocation] = useState('');
+
+
+
+  useEffect(() => {
+
+    async function fetchData() {
+      try {
+        const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location},nl&appid=${process.env.REACT_APP_API_KEY}&lang=nl`);
+        console.log(result.data);
+        setWeatherData(result.data);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
+    if (location) {
+      fetchData();
+    }
+
+  }, [location]);
+
   return (
     <>
       <div className="weather-container">
 
         {/*HEADER -------------------- */}
         <div className="weather-header">
-          <SearchBar/>
+          <SearchBar setLocationHandler={setLocation} />
 
           <span className="location-details">
-            <h2>Bewolkt</h2>
-            <h3> </h3>
-            <h1>14 &deg;</h1>
-
-            <button type="button">
-              Haal data op!
-            </button>
+            {weatherData &&
+              <>
+                <h2>{weatherData.weather[0].description}</h2>
+                <h3>{weatherData.name}</h3>
+                <h1>{weatherData.main.temp}</h1>
+              </>
+            }
           </span>
         </div>
 
         {/*CONTENT ------------------ */}
         <div className="weather-content">
-          <TabBarMenu/>
+          <TabBarMenu />
 
           <div className="tab-wrapper">
-            Alle inhoud van de tabbladen komt hier!
+            <ForecastTab coordinates={weatherData && weatherData.coord} />
           </div>
         </div>
 
-        <MetricSlider/>
+        <MetricSlider />
       </div>
     </>
   );
--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchBar.css';
 
-function SearchBar() {
+function SearchBar({ setLocationHandler }) {
+
+  const [query, setQuery] = useState('');
+
+  function onClick() {
+    setLocationHandler(query);
+  }
+  function keyPressCheck(e) {
+    if (e.keyCode === 13) {
+      setLocationHandler(query)
+    }
+  }
+
   return (
     <span className="searchbar">
       <input
         type="text"
         name="search"
         placeholder="Zoek een stad in Nederland"
+        onChange={(e) => setQuery(e.target.value)}
+        value={query}
+        onKeyUp={keyPressCheck}
       />
 
-      <button type="button">
+      <button type="button" onClick={onClick}>
         Zoek
       </button>
     </span>
